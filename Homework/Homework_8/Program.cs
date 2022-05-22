83c30ad84c7da42ce87ef4190240246efae04fac
Kiasu <pogonyalin@mail.ru>
2022-05-22 21:30:04 +0300

    Homework 8 is half done!

---
+++ Homework/Homework_8/Program.cs
@@ -0,0 +1,110 @@
+﻿/*
+//Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по возрастанию элементы каждой строки двумерного массива.
+
+void FillArray(int[,] array)
+{
+    for (int m = 0; m < array.GetLength(0); m++)
+    {
+        for (int n = 0; n < array.GetLength(1); n++)
+        {
+            array[m, n] = new Random().Next(1, 10);
+        }
+    }
+}
+void PrintArray(int[,] array)
+{
+    for (int m = 0; m < array.GetLength(0); m++)
+    {
+        for (int n = 0; n < array.GetLength(1); n++)
+        {
+            Console.Write($"{array[m, n]} ");
+        }
+        Console.WriteLine();
+    }
+}
+void Ordering(int[,] matr)
+{
+    for (int m = 0; m < matr.GetLength(0); m++)
+    {
+        for (int n = 0; n < matr.GetLength(1); n++)
+        {
+            for (int k = 0; k < matr.GetLength(1) - n - 1; k++)
+            {
+                if (matr[m, k] > matr[m, k + 1])
+                {
+                    int t = matr[m, k];
+                    matr[m, k] = matr[m, k + 1];
+                    matr[m, k + 1] = t;
+                }
+            }
+        }
+    }
+}
+
+int[,] matrix = new int[5, 5];
+FillArray(matrix);
+PrintArray(matrix);
+Console.WriteLine();
+Ordering(matrix);
+PrintArray(matrix);
+*/
+
+/*
+//Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+void FillArray(int[,] matr)
+{
+    for (int m = 0; m < matr.GetLength(0); m++)
+    {
+        for (int n = 0; n < matr.GetLength(1); n++)
+        {
+            matr[m, n] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] matr)
+{
+    for (int m = 0; m < matr.GetLength(0); m++)
+    {
+        for (int n = 0; n < matr.GetLength(1); n++)
+        {
+            Console.Write($"{matr[m, n]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int Sum(int[,] matr)
+{
+    int sum = 0;
+    int minSum = 0;
+    int minNum = 0;
+    for (int m = 0; m < matr.GetLength(0); m++)
+    {
+        for (int n = 0; n < matr.GetLength(1); n++)
+        {
+            if (m == 0) 
+            {
+                sum += matr[m, n];
+                minSum += matr[m, n]; 
+            }
+            else sum += matr[m, n]; 
+        }
+        if (sum < minSum)
+        {
+            minSum = sum;
+            minNum = m;
+        }
+        sum = 0;
+    }
+    return minNum;
+}
+int[,] matrix = new int[3, 4];
+FillArray(matrix);
+PrintArray(matrix);
+Console.WriteLine();
+Console.WriteLine("Cтрока с наименьшей суммой элементов: " + Sum(matrix));
+*/
+
+//Задача 62. Заполните спирально массив 4 на 4. 
